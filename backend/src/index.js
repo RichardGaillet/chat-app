e970eb50cd1c64a8765a1da81e5922cd379fc911
RichardGaillet <gaillet.richard@gmail.com>
2025-04-30 08:48:08 +0200

    Add messages functionality.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import express from "express"
 
 import { connectDB } from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 
 dotenv.config()
 
@@ -15,6 +16,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(5001, () => {
   console.log(`Server is running on port ${PORT}`)
