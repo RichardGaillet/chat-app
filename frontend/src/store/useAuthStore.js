2ed8f51d7f05bca32ef1224c08b0b09e049a0d9c
RichardGaillet <gaillet.richard@gmail.com>
2025-05-12 20:11:37 +0200

    Add sidebar component.

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -10,6 +10,8 @@ export const useAuthStore = create((set) => ({
 
   isCheckingAuth: true,
 
+  onlineUsers: [],
+
   checkAuth: async () => {
     try {
       const response = await axiosInstance.get("/auth/check")
